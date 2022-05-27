87c5c17ccdb2d9f128111b7dfb2728fc5420c065
Joyce Park <joycepark@troutgirl.local>
2022-05-26 17:23:27 -0700

    These changes will make the sample easier to stand up, the old config had weird values and the documentation was inadequate

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 const config = {
   // OAuth info (copied from the FusionAuth admin panel)
-  clientID: '48f671cc-1627-41b5-aed1-260b80a9f742',
-  clientSecret: 'pqQj3uOj7q7BLe0ImvgSxCvKUTuczAth1vjAmkntve8',
+  clientID: '85a03867-dccf-4882-adde-1a79aeec50df',
+  clientSecret: '6HvlAe28V4PL9OEKJ0VlZ6ZsEAluEsZHVFYKuoLAr1U',
   redirectURI: 'http://localhost:3001/oauth-callback',
-  applicationID: '48f671cc-1627-41b5-aed1-260b80a9f742',
+  applicationID: '85a03867-dccf-4882-adde-1a79aeec50df',
 
   // our FusionAuth api key
   apiKey: 'bf69486b-4733-4470-a592-f1bfce7af580',
